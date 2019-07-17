1878231118bbb07772a2ce50b8873648f2a64a35
Mike <michaelamparo@pursuit.org>
2019-07-16 22:04:58 -0400

    edit

--- functions_and_scope_lab.js
+++ functions_and_scope_lab.js
@@ -13,11 +13,7 @@ Input: '12, 4'
 Output: '12'
 \n--------------------------------------\n\n`);
 
-function larger(x, y) {
-  let z;
-  x > y ? z = x : z = y;
-  return z;
-}
+const larger = (x, y) => x > y ? z = x : z = y;
 
 console.log(larger(12, 4));
 
@@ -33,9 +29,7 @@ Input: '4'
 Output: '28'
 \n--------------------------------------\n\n`);
 
-function getDogAge(humanAge) {
-  return humanAge * 7;
-}
+const getDogAge = (humanAge) => humanAge * 7;
 
 console.log(getDogAge(4));
 
@@ -51,13 +45,13 @@ Input: '[13,2,4,-2]'
 Output: '17'
 \n--------------------------------------\n\n`);
 
-function arraySum(arr) {
+const arraySum = (arr) => {
   sum = 0;
   for (let i of arr) sum += i;
   return sum;
 }
 
-console.log(arraySum([13,2,4,-2]));
+console.log(arraySum([13, 2, 4, -2]));
 
 //## Question 4
 console.log(`\n\n\n\n========================================\n## Question 4\n===============\n`);
@@ -71,9 +65,7 @@ Input: '14, 5'
 Output: '19'
 \n--------------------------------------\n\n`);
 
-function sumOfTwo(x, y) {
-  return x + y;
-}
+const sumOfTwo = (x, y) => x + y;
 
 console.log(sumOfTwo(14, 5));
 
@@ -99,7 +91,7 @@ Input: '88'
 Output: '"B"'
 \n--------------------------------------\n\n`);
 
-function letterGrade(number) {
+const letterGrade = (number) => {
   if (number >= 100) return `A+`;
   else if (number >= 90) return `A`;
   else if (number >= 80) return `B`;
@@ -110,8 +102,8 @@ function letterGrade(number) {
 
 console.log(letterGrade(88));
 
-//## Question 9
-console.log(`\n\n\n\n========================================\n## Question 9\n===============\n`);
+//## Question 6
+console.log(`\n\n\n\n========================================\n## Question 6\n===============\n`);
 console.log(`
 Implement a function named 'repeatLog' that takes a string and a number as parameters. The function should log 'message' 'count' number of times.
 
@@ -123,7 +115,7 @@ Output: '++++++++++'
 
 \n--------------------------------------\n\n`);
 
-function repeatLog(str, n) {
+const repeatLog = (str, n) => {
   let string = ``;
   for (let i = n; i > 0; i--) string += str;
   console.log(string);
@@ -131,8 +123,8 @@ function repeatLog(str, n) {
 
 repeatLog(`+`, 10);
 
-//## Question 10
-console.log(`\n\n\n\n========================================\n## Question 10\n===============\n`);
+//## Question 7
+console.log(`\n\n\n\n========================================\n## Question 7\n===============\n`);
 console.log(`
 Write a function named 'first' that takes a number and returns an array with all the numbers from 1 to n
 
@@ -145,7 +137,7 @@ Output: '[1, 2, 3]'
 
 \n--------------------------------------\n\n`);
 
-function first(n) {
+const first = (n) => {
   let arr = [];
   for (let i = 1; i <= n; i++) arr.push(i);
   return arr;
@@ -153,8 +145,8 @@ function first(n) {
 
 console.log(first(3));
 
-//## Question 11
-console.log(`\n\n\n\n========================================\n## Question 11\n===============\n`);
+//## Question 8
+console.log(`\n\n\n\n========================================\n## Question 8\n===============\n`);
 console.log(`
 Write a function that logs the numbers from 1 to x, except:
 
@@ -165,19 +157,21 @@ Your function should take in one parameter: x (the number to count up to)
 
 \n--------------------------------------\n\n`);
 
-function fizzBuzz(x) {
-  for (let i = 1; i <= x; i++) {
-    if (!(i % 15)) console.log(`FizzBuzz`);
+const fizzBuzz = (x) => {
+  let i = 1
+  while (i <= x) {
+    if      (!(i % 15)) console.log(`FizzBuzz`);
     else if (!(i % 3)) console.log(`Fizz`);
     else if (!(i % 5)) console.log(`Buzz`);
     else console.log(i);
+    i++;
   }
 }
 
 fizzBuzz(20);
 
-//## Question 12
-console.log(`\n\n\n\n========================================\n## Question 12\n===============\n`);
+//## Question 9
+console.log(`\n\n\n\n========================================\n## Question 9\n===============\n`);
 console.log(`
 Write a function named 'reverse' that takes an array as a parameter. The function should return an array in reverse order.
 
@@ -190,7 +184,7 @@ Output: '[3, 2, 1]'
 
 \n--------------------------------------\n\n`);
 
-function reverse(arr) {
+const reverse = (arr) => {
   let rArr = [];
   for (let i of arr) rArr.unshift(i);
   return rArr;
@@ -198,8 +192,8 @@ function reverse(arr) {
 
 console.log(reverse([1, 2, 3]));
 
-//## Question 13
-console.log(`\n\n\n\n========================================\n## Question 13\n===============\n`);
+//## Question 10
+console.log(`\n\n\n\n========================================\n## Question 10\n===============\n`);
 console.log(`
 Write a function that prints out the most frequently element in an array.
 
@@ -212,7 +206,7 @@ Output: '2'
 
 \n--------------------------------------\n\n`);
 
-function mostFreq(arr) {
+const mostFreq = (arr) => {
   let obj = {};
   let count = 0;
   let mostFreq = ``;
@@ -229,8 +223,8 @@ function mostFreq(arr) {
 
 mostFreq([2,1,4,2,5,2,3,23,3,3,2,2,2,24,56,2,2,5,46,2]);
 
-//## Question 14
-console.log(`\n\n\n\n========================================\n## Question 14\n===============\n`);
+//## Question 11
+console.log(`\n\n\n\n========================================\n## Question 11\n===============\n`);
 console.log(`
 Write a function that sums all the even numbers in an array.
 
@@ -242,7 +236,7 @@ Output: '88'
 
 \n--------------------------------------\n\n`);
 
-function evenSum(arr) {
+const evenSum = (arr) => {
   let evenSum = 0;
   for (let i of arr) !(i % 2) ? evenSum += i : null;
   return evenSum;
@@ -250,8 +244,8 @@ function evenSum(arr) {
 
 console.log(evenSum([1,2,5,64,23,22,91,3]));
 
-//## Question 15
-console.log(`\n\n\n\n========================================\n## Question 15\n===============\n`);
+//## Question 12
+console.log(`\n\n\n\n========================================\n## Question 12\n===============\n`);
 console.log(`
 Write a function that flips a dictionary.  All of the keys are now values and all of the values are now keys.
 
@@ -263,7 +257,7 @@ Output: '{"uno": 1, "cinco": 5}'
 
 \n--------------------------------------\n\n`);
 
-function objFlip(obj) {
+const objFlip = (obj) => {
   let flipObj = {};
   for (let i in obj) flipObj[obj[i]] = i;
   return flipObj;
@@ -271,8 +265,8 @@ function objFlip(obj) {
 
 console.log(objFlip({1: "uno", 5: "cinco"}));
 
-//## Question 16
-console.log(`\n\n\n\n========================================\n## Question 16\n===============\n`);
+//## Question 13
+console.log(`\n\n\n\n========================================\n## Question 13\n===============\n`);
 console.log(`
 Write a function that determines if a value is inside of array.
 
@@ -283,7 +277,7 @@ Input: '[1,4,6,9,10], 5'
 Output: 'false'
 \n--------------------------------------\n\n`);
 
-function valCheck(arr, val) {
+const valCheck = (arr, val) => {
   for (let i of arr) if (i === val) return true;
   return false;
 }
