23cd491bf65a1bc4d1e233fe8c27722a50cf7a32
joseph-p-pasaoa <52214348+joseph-p-pasaoa@users.noreply.github.com>
2019-07-29 09:36:17 -0400

    v.5, minor revisions & edit via browser test

--- functions_and_scope_lab.js
+++ functions_and_scope_lab.js
@@ -3,8 +3,8 @@
 
 
 
-// global function declarations
-const spacer = () => {console.log("\n")}
+// non-question-specific function declarations
+const spacer = () => {console.log("")};
 
 
 
@@ -17,7 +17,7 @@ const whosLarger = (num1, num2) => {
 }
 
 // f.2
-const getDogAge = (num1) => num1 * 7
+const getDogAge = (num1) => num1 * 7;
 
 // f.3
 const arraySum = (input_Arr) => {
@@ -74,9 +74,9 @@ const bizzFuzzer = (max) => {
   for (let i = 1; i <= max; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log(`FizzBuzz`);
-    } else if (!(i % 3)) {
+    } else if ( !(i % 3) ) {
       console.log(`Fizz`);
-    } else if (!(i % 5)) {
+    } else if ( !(i % 5) ) {
       console.log(`Buzz`);
     } else {
       console.log(i);
@@ -112,10 +112,10 @@ const getMostElemOfArr = (input_Arr) => {
 
 // f.11
 const sumOfTheEvens = (input_Arr) => {
-  let sum = 0;
+  let outputSum = 0;
   for (let num of input_Arr) {
     if (!Math.abs(num % 2)) {
-      sum += num;
+      outputSum += num;
     }
   }
   return sum;
@@ -131,9 +131,9 @@ const flipTheObject = (input_Obj) => {
 }
 
 // f.13
-const isElemPresent = (input_Arr, findMeNum) => {
+const isElemPresent = (input_Arr, findMyNum) => {
   for (let el of input_Arr) {
-    if (el === findMeNum) {
+    if (el === findMyNum) {
       return true;
     }
   }
@@ -144,7 +144,7 @@ const isElemPresent = (input_Arr, findMeNum) => {
 
 // stage
 
-// 1
+// 1 //
 console.log("1. Write a function that returns the largest of two numbers. If they are equal, return the second one.");
 let labInput1a = 1;
 let labInput1b = 4;
@@ -156,7 +156,7 @@ console.log(result1);
 spacer();
 
 
-// 2
+// 2 //
 console.log("2. Write a function named getDogAge that takes one argument: a dog's age in human years. The function returns the dog's age in dog years. The conversion rate is 1 human year to 7 dog years.");
 let labInput2 = 5;
 console.log(`${labInput2} ->`);
@@ -167,7 +167,7 @@ console.log(result2);
 spacer();
 
 
-// 3
+// 3 //
 console.log("3. Write a function `arraySum` that takes in an array and returns the sum of all the elements.");
 let labInput3 = [4, 30, 200, 1000, -9];
 console.log(labInput3);
@@ -178,7 +178,7 @@ console.log(result3);
 spacer();
 
 
-// 4
+// 4 //
 console.log("4. Write a function that takes in any two numbers and return their sum.");
 let labInput4a = 60;
 let labInput4b = -31;
@@ -190,7 +190,7 @@ console.log(result4);
 spacer();
 
 
-// 5
+// 5 //
 console.log("5. Write a function takes in any number grade and returns a corresponding letter grade.");
 let labInput5 = 75;
 console.log(labInput5);
@@ -201,7 +201,7 @@ console.log(result5);
 spacer();
 
 
-// 6
+// 6 //
 console.log("6. Implement a function named `repeatLog` that takes a string and a number as parameters. The function should log `message` `count` number of times.");
 let labInput6_str = "gamma";
 let labInput6_num = 5;
@@ -213,7 +213,7 @@ console.log(result6);
 spacer();
 
 
-// 7
+// 7 //
 console.log ("7. Write a function named `first` that takes a number and returns an array with all the numbers from 1 to n");
 let labInput7 = 11;
 console.log(labInput7);
@@ -224,7 +224,7 @@ console.log(result7);
 spacer();
 
 
-// 8
+// 8 //
 console.log("8. Write a function that logs the numbers from 1 to x, except: mults of 3 log 'Fizz', mults of 5 log 'Buzz', mults of 3 & 5 log 'FizzBuzz'. Take in: x (the number to count up to).");
 let x = 25;
 console.log(`BIZZFUZZING up to: ${x}`);
@@ -234,7 +234,7 @@ bizzFuzzer(x);
 spacer();
 
 
-// 9
+// 9 //
 console.log("9. Write a function named `reverse` that takes an array as a parameter. The function should return an array in reverse order.");
 let labInput9 = [8,7,6,5,-8,-7,-6,-5];
 console.log(labInput9);
@@ -245,7 +245,7 @@ console.log(result9);
 spacer();
 
 
-// 10
+// 10 //
 console.log("10. Write a function that prints out the most frequently occurring element in an array.");
 let labInput10 = [2,1,4,2,5,2,3,23,0,0,0,0,0,0,0,0,0,0,3,3,2,2,2,24,56,2,2,5,46,2];
 console.log(labInput10);
@@ -256,7 +256,7 @@ console.log(`${result10} is the most recurring element.`);
 spacer();
 
 
-// 11
+// 11 //
 console.log("11. Write a function that sums all the even numbers in an array.");
 let labInput11 = [1,2,5,64,23,22,91,3];
 console.log(labInput11);
@@ -267,7 +267,7 @@ console.log(result11);
 spacer();
 
 
-// 12
+// 12 //
 console.log("12. Write a function that flips an object.  All of the keys are now values and all of the values are now keys.");
 let labInput12 = {
   "uno": "red",
@@ -284,7 +284,7 @@ console.log(result12);
 spacer();
 
 
-// 13
+// 13 //
 console.log("13. Write a function that determines if a value is inside of array.");
 let labInput13_arr = [1,2,5,64,23,22,91,3];
 let labInput13_num = 91;
