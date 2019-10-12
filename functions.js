9e83e2c01959eef7cfc656bcfc969fa6444b5076
dannyl <danilovlaurent@pursuit.org>
2019-10-12 16:05:58 -0400

    problem 2

--- functions.js
+++ functions.js
@@ -1,4 +1,18 @@
-function double (num) {
-    return num * 2;
+// PROBLEM 1
+
+// function double (num) {
+//     return num * 2;
+// }
+// console.log(double(99));
+
+
+// PROBLEM 2
+
+function smallest (num1, num2) {
+    if ( num1 < num2) {
+        return num1 ;
+    } else {
+        return num2
+    }
 }
-console.log(double(99));
+console.log(smallest(2.3, 2.03))
