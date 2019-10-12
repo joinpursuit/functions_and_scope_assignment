7396ca8d625fb1fcc5faaeaea030be62e2fe201b
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-12 16:58:38 -0400

    question 3

--- functionLab.js
+++ functionLab.js
@@ -15,3 +15,17 @@ function smallest(num1, num2){
 console.log(smallest(10, 5))
 
 //Question 3
+array = [1,2,3,4,5,5]
+function smallestValueInArr(array){
+    let smallNum = 0
+    for(let i = 0; i < array.length; i++){
+         if (array[i]) {
+        smallNum = array[i]
+
+    } 
+    }  
+    return smallNum
+} 
+console.log(smallestValueInArr(array))
+
+//Question 4
