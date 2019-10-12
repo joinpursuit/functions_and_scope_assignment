f60e51186638f2e09ab38e5452c237d1a2c5b45d
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-10-12 16:04:58 -0400

    solve prob 2

--- functions.js
+++ functions.js
@@ -6,3 +6,15 @@
 // }
 // console.log(double(2))
 
+// ## Question Two
+// Write a function named smallest that takes in two numbers and returns the smaller number
+
+function smallest(num1, num2) {
+    if (num1 < num2) {
+        return num1 
+    }
+    else {
+        return num2
+    }
+}
+console.log(smallest(3, 4))
