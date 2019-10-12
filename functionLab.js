090ad37559588bf2e4a6d57d0b8b67166950f5db
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-12 16:24:30 -0400

    question 2

--- functionLab.js
+++ functionLab.js
@@ -1,7 +1,17 @@
-//Question 1
+// //Question 1
 function double(num) {
     return num * 2
 }
 console.log(double(2))
 
-//Qustion 2
+//Question 2
+function smallest(num1, num2){
+    if(num1 > num2){
+        return num2
+    } else {
+        return num1
+    }
+}
+console.log(smallest(10, 5))
+
+//Question 3
