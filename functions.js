994a92521f77d053c6de275ec8bb76ad2ff81776
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-10-12 16:18:59 -0400

    solve prob 3

--- functions.js
+++ functions.js
@@ -9,12 +9,30 @@
 // ## Question Two
 // Write a function named smallest that takes in two numbers and returns the smaller number
 
-function smallest(num1, num2) {
-    if (num1 < num2) {
-        return num1 
+// function smallest(num1, num2) {
+//     if (num1 < num2) {
+//         return num1 
+//     }
+//     else {
+//         return num2
+//     }
+// }
+// console.log(smallest(3, 4))
+
+// ## Question Three
+// Write a function named smallestValueInArr that takes in an array of numbers and returns the smallest number
+
+arr1 = [1.0,2,3,4,5,5]
+arr2 = [6,5,4,3,2,1]
+arr3 = [-4,-59,-348,-34,-4]
+
+function smallestValueInArr(arr) {
+    let smallestNum = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (smallestNum > arr[i]) {
+            smallestNum = arr[i];
+        }
     }
-    else {
-        return num2
-    }
+    return smallestNum;
 }
-console.log(smallest(3, 4))
+console.log(smallestValueInArr(arr3));
