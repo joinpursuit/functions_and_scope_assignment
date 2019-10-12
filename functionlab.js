2e39c28981c77bc105d25790b6f195118256aa09
rafidhossain1995 <rafidhossain@pursuit.org>
2019-10-12 16:52:24 -0400

    question 5

---
+++ functionlab.js
@@ -0,0 +1,61 @@
+
+
+// // 1)
+// function double(number){
+//     return number * 2
+// }
+// console.log(double(10))
+
+// //2
+
+// function smallest(num1, num2){
+//     let small = 0
+//     if (num1 < num2){
+//     small = num1
+//     } else {
+//         (small = num2)
+//     }
+//     return small
+// }
+// console.log(smallest(9,8))
+
+// // 3
+
+// let arr1 = [6, 5, 4, 3, 2, 1, -5]
+// let smallValue = Infinity
+// function smallestValueInArr(arr){
+//     for(i = 0; i < arr.length; i++){
+//     if( arr[i] < smallValue )
+//     smallValue = arr[i]
+//     }
+//     return smallValue
+// }
+// console.log(smallestValueInArr(arr1))
+
+
+// //4
+// let word = "Hello"
+// function occurences(string){
+//     let counter = 0
+//     for (i = 0; i < word.length; i++)
+//     if(word[i] === string)
+//         counter += 1
+//      return counter
+// }
+// console.log(occurences("l"))
+
+
+// //5 
+// let arr = [1, 42, 1, 541, 42, 6]
+// function average(arr) {
+// let sum = 0
+// for (i = 0; i < arr.length; i++)
+//     sum += arr[i]
+// return sum/arr.length
+// }
+// console.log(average(arr))
+
+//6
+let word = "Hello"
+console.log(Object.keys("Hello"))
+
