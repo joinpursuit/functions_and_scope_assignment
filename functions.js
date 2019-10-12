f87d338a0f1cc662d6e26b7a0a3541aa691118cd
dannyl <danilovlaurent@pursuit.org>
2019-10-12 16:42:28 -0400

    problem 3

--- functions.js
+++ functions.js
@@ -8,11 +8,30 @@
 
 // PROBLEM 2
 
-function smallest (num1, num2) {
-    if ( num1 < num2) {
-        return num1 ;
-    } else {
-        return num2
-    }
-}
-console.log(smallest(2.3, 2.03))
+// function smallest (num1, num2) {
+//     if ( num1 < num2) {
+//         return num1 ;
+//     } else {
+//         return num2
+//     }
+// }
+// console.log(smallest(2.3, 2.03))
+
+
+// PROBLEM 3
+
+// let arr1 = [1.0, 2, 3, 4, 5, 5] 
+// let arr2 = [6, 5, 4, 3, 2, 1]
+// let arr3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+// let arr4 = [-4, -59, -348, -34, -4]
+// function smallestValueInArr (arrNum){
+//     let smallestNumber = arrNum[0] 
+//     for( let i = 0; i < arrNum.length; i++){
+//         if (arrNum[i]  < smallestNumber){
+//             smallestNumber = arrNum[i];
+//         }
+//         // console.log(i)
+//     } 
+//     return smallestNumber;
+// }
+// console.log(smallestValueInArr(arr4));
