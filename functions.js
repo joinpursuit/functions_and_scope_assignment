d78d8d50d6137bc0b897f0644ac0caa2dba0607d
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-10-12 16:01:15 -0400

    solve prob1

---
+++ functions.js
@@ -0,0 +1,8 @@
+// ## Question One
+// Write a function named double that takes in a number and returns that number times two
+
+// function double(a) {
+//     return a * 2;
+// }
+// console.log(double(2))
+
