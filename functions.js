578c00fe0ba5f0a42ba438bd4eba6ebb7203bd7e
Ohidurr <Ohidurrahman96@gmail.com>
2019-10-13 14:52:35 -0400

    redo functions lap with new question set

--- functions.js
+++ functions.js
@@ -1,197 +1,138 @@
-/* ## Question 1
-
-Write a function that returns the largest of two numbers. If they are equal, return the second one.
-
-Example:
-
-Input: `12, 4`
-
-Output: `12`
-*/
-console.log('#1');
-
-function twoLargestNum(numbers) {
-  numbers.sort(function(a,b){
-    return b - a;
-  });
-  return numbers[0] + numbers[1]
+// question 1
+function double(num) {
+  return num * 2;
 }
-let numbers = twoLargestNum([10,20,30,40,50])
-console.log(numbers);
-
-/*
-## Question 2
-
-Write a function named getDogAge that takes one argument: a dog's age in human years. The function returns the dog's age in dog years. The conversion rate is 1 human year to 7 dog years.
-
-Example:
-
-Input: `4`
-
-*/
-
-console.log(" ");
-console.log('#2');
-
-function getDogAge (age){
-  return age*7;
-}
-let age = getDogAge(10);
-console.log(age);
-
-/*## Question 3
-
-Write a function `arraySum` that takes in an array and returns the sum of all the elements.
-
-Example:
-
-Input: `[13,2,4,-2]`
-Output: `17`
-*/
-
-console.log(" ");
-console.log("#3");
-
-const arraySum = (arr) =>{
-  let sums = 0
-  for(let i = 0; i <= arr.length-1; i++){
-    sums+= arr[i];
-   }
-   return sums
- }
-let result = arraySum([1,2,3,4,5])
- console.log(result);
-
-/*
-## Question 4
-Write a function that takes in any two numbers and return their sum.
-
-Example:
-
-Input: `14, 5`
-
-Output: `19`
-*/
-console.log(" ");
-console.log("#4");
-
-function add(array){
-  let sum = 0
-  for(let i = 0; i <= array.length-1;i++){
-    sum = array[2]+array[3];
-    }
-      return sum;
-}
-let result2 = add([0,1,3,7,11])
-console.log(result2);
-
-/*
-## Question 5
-
-Write a function takes in any number grade and returns a corresponding letter grade.
-
-| Number Grade | Equivalent Letter Grade |
-| :--------: | :---------: |
-| 100 | A+ |
-| 90 - 99 | A |
-| 80 - 89 | B |
-| 70 - 79 | C |
-| 65 - 69 | D |
-| Below 65 | F |
-
-
-Example:
-
-Input: `88`
-
-Output: `"B"`
-*/
-
+console.log(double(4));
 console.log("");
-console.log('#5');
-
-const grade = (letter) => {
-   value = 0;
-   if(grade >= 100){
-    return value = "A+" //console.log("A+");
-   }
-   else if (letter <= 90 && letter >= 99 ) {
-    return value = "A" //console.log('A');
-
-   }
-   else if (letter >= 80 && letter <= 89 ) {
-     return value = 'B'//console.log('B');
-}
-  else if (letter >= 70 && letter <= 79 ) {
-  return value = 'C'//console.log('C');
-}
-  else if (letter >= 65 && letter <= 69 ) {
-  return value = 'D'//console.log('D');
-}
-  else{
-  return value = 'F'//console.log('F');
-}
-}
-  let grades = grade(88.8);
-console.log(grades);
 
 
-/*
-## Question 6
-
-Implement a function named `repeatLog` that takes a string and a number as parameters. The function should log `message` `count` number of times.
-
-Example:
-
-Input: `"+", 10`
-
-Output: `++++++++++`
-*/
-console.log('');
-console.log('#6');
-    function repeatLog (str,rep){
-      let result = "";
-      for(let i = 0; i < rep; i++){
-        result += str;
-}
-  return result;
-}
-  let log = repeatLog("+",10)
-console.log(log);
-/*
-# Question 7
-
-Write a function named `first` that takes a number and returns an array with all the numbers from 1 to n
-
-
-Example:
-
-Input: `first(3)`
-
-Output: `[1, 2, 3]`
-*/
-console.log('');
-console.log("#7");
-function first (arr){
-
-}
-
-/*
-## Question 8
-
-Write a function that logs the numbers from 1 to x, except:
-
-If the number if a multiple of 3, log `"Fizz"` instead of the number
-If the number is a multiple of 5, log `"Buzz"` instead of the number
-If the number is a multiple of 3 AND 5, log `"FizzBuzz"` instead of the number
-Your function should take in one parameter: x (the number to count up to)
-*/
-
-
-const fizzbuzz = () =>{
-  for(let i = 0; i < 100; i++){
-
+//question 2
+function smallest(num1,num2) {
+  if (num1 > num2){
+      return num2
+  }else {
+      return num1
   }
 }
+console.log(smallest(0,3));
+console.log("");
 
-//was not able to fnish lab. this week was heecktic for me. I will submit for now, and will push the additional answers. 
+
+//question 3
+function smallestValueInArr(arr) {
+  let num  = arr[0] 
+  for(let i = 0; i < arr.lengh; i++){
+      if(arr[i] < num){
+          num = arr[i]
+      }
+  
+  }
+  return num;
+}
+console.log(smallestValueInArr([1.0,2,3,4,5,5]));
+console.log("");
+
+//question 4
+// let mystr = "Hello World"
+function occurrences (char,str){
+  let count = 0 
+ for (let i = 0; i < str.length; i++){
+  if(char === str[i]){
+      count++
+ }
+}
+return count;
+}
+console.log(occurrences("l","Hello World")); //3
+console.log("");
+
+
+//question 5
+
+function avg(arr) {
+  sum = 0
+  for (let i = 0; i < arr.length; i++) {
+      sum += arr[i]
+  }
+  return sum / arr.length
+}
+console.log(avg([1,2,3,4,5])) //3
+console.log("");
+
+
+
+
+//question 6 
+const  frequencyMap =(str) => {
+  
+  let map = {};
+  for(let i = 0; i < str.length; i++){
+      let char = str.charAt(i);
+      if(map[char]){
+          map[char]++;
+     }
+     else {
+         map[char] = 1;
+     }
+
+  }
+  return map;
+}
+console.log(frequencyMap("Hello"));
+
+//question 7
+console.log("");
+
+
+function theN(n){
+  for (let i = 1; i <= n; i++) { 
+      if (i % 3 === 0 && i % 5 === 0){
+          console.log ("FizzBuzz")
+      } else if (i % 3 === 0) {
+          console.log("Fizz")
+      } else if (i % 5 === 0){
+          console.log("Buzz")
+      } else {
+          console.log(i);
+      }
+  }
+}
+  theN(20);
+
+//question 8
+console.log("");
+
+function valueIsGreaterThanAverageOfArr(arr) {
+      sum = 0
+      for (let i = 0; i < arr.length; i++) {
+          sum += arr[i]
+      }
+  if ((sum / arr.length) > (sum * 2)){
+      return "the average is bigger"
+  } else {
+      return "the double is bigger"
+  }
+}
+ console.log(valueIsGreaterThanAverageOfArr([1,2,3,4,5]));
+
+ //question 9;
+ console.log("");
+ 
+ const secondSmall = (arr) => {
+  let smallest = Infinity
+  let secondSmallest = Infinity
+     for(let i = 0; i < arr.length; i++){
+          if(arr[i] < smallest){
+              smallest = arr[i]
+          }
+          else if(arr[i] < secondSmallest && arr != smallest){
+              secondSmallest = arr[i]
+          }
+      }
+
+      return secondSmallest
+  }
+ console.log(secondSmall([7,1,5,3,4,5]));
+
+ // Ohidur R.
+ // Syn P.
