e568c59716bff853c3e6d772d1ccd6cb056e647c
dannyl <danilovlaurent@pursuit.org>
2019-10-13 16:05:30 -0400

    problem 9 done ES5

--- functions.js
+++ functions.js
@@ -72,25 +72,25 @@
 
 // PROBLEM 6
 
-let str1 = "hello"
-let str2 = "aaaaaAAA"
-let str3 = "More words"
+// let str1 = "hello"
+// let str2 = "aaaaaAAA"
+// let str3 = "More words"
 
 
-function frequencyMap(str) {
-    let newObj = {};
-    for(let i = 0; i < str.length; i++){
-        if (newObj[str[i]]){
-            newObj[str[i]] +=1
-             } else {
-                newObj[str[i]] = 1
+// function frequencyMap(str) {
+//     let newObj = {};
+//     for(let i = 0; i < str.length; i++){
+//         if (newObj[str[i]]){
+//             newObj[str[i]] +=1
+//              } else {
+//                 newObj[str[i]] = 1
                 
-            }  
-        }
+//             }  
+//         }
         
-        return newObj
-    }
-console.log(frequencyMap(str1))
+//         return newObj
+//     }
+// console.log(frequencyMap(str1))
 
 
 
@@ -145,23 +145,22 @@ console.log(frequencyMap(str1))
 
 // PROBLEM 9
 
-// let arr1 = [1,2,3,4]
-// let arr4 = [807,909,-22,424,244,873,-907,-350,-780,576,-177,278,-855,296,-33,-609,-664,-126,-469,-57,-376,-424,-477,-795,-481,-806,545,727,-879,210,-114,-783,-156,45,781,369,35,900,904,-2,168,489,-329,608,-287,40,442,362,-560,236,-583,698,544,-154,478,535,-397,363,470,-49,49,631,-62,-293,-419,-528,753,-809,-48,-888,606,-889,-908,672,-783,-921,-198,-428,701,-30,106,500,-106,199,-793,392,873,330,366,45,840,649,-135,-850,174,-338,901,-753,402,-62,554,-579,863,955,-999,-13,851,760,-523,-968,225,-173,-605,-759,306,657,844,168,-270,883,963,-835,624,570,705,333,-293,283,382,926,839,264,602,-940,160,-369,-770,-792,722,321,88,-176,-911,203,486,-175,-529,187,-668,518,-282,162,-165,-34,544,538,-828,-562,-154,591,345,-241,962,801,-632,-921,-710,588,-642,113,128,-242,56,716,-709,232,-870,241,-612,-583,-199,762,-601,-410,-896,-493,664,531,-613,-285,-951,-589,243,819,-195,911,-701,-368,-85,-316]
-//   function smallestt(array){
-//    let smallest = array[0];
-//    let secSmallest = array[0];
-//    for(let i = 0; i <= array.length; i += 1){
-//        if(array[i] < smallest){
-//            smallest = array[i]
-//         if (array[i] < secSmallest && secSmallest > smallest){
-            
-//              secSmallest = array[i];
-//        }
-//    }
-// }
-// return secSmallest
-// }
-// console.log(smallestt(arr4));
+let arr1 = [1,2,3,4]
+let arr4 = [807,909,-22,424,244,873,-907,-350,-780,576,-177,278,-855,296,-33,-609,-664,-126,-469,-57,-376,-424,-477,-795,-481,-806,545,727,-879,210,-114,-783,-156,45,781,369,35,900,904,-2,168,489,-329,608,-287,40,442,362,-560,236,-583,698,544,-154,478,535,-397,363,470,-49,49,631,-62,-293,-419,-528,753,-809,-48,-888,606,-889,-908,672,-783,-921,-198,-428,701,-30,106,500,-106,199,-793,392,873,330,366,45,840,649,-135,-850,174,-338,901,-753,402,-62,554,-579,863,955,-999,-13,851,760,-523,-968,225,-173,-605,-759,306,657,844,168,-270,883,963,-835,624,570,705,333,-293,283,382,926,839,264,602,-940,160,-369,-770,-792,722,321,88,-176,-911,203,486,-175,-529,187,-668,518,-282,162,-165,-34,544,538,-828,-562,-154,591,345,-241,962,801,-632,-921,-710,588,-642,113,128,-242,56,716,-709,232,-870,241,-612,-583,-199,762,-601,-410,-896,-493,664,531,-613,-285,-951,-589,243,819,-195,911,-701,-368,-85,-316]
+  function smallestt(array){
+   let smallest = array[0];
+   let secSmallest = array[0];
+   for(let i = 0; i <= array.length; i += 1){
+       if(array[i] < smallest){
+           smallest = array[i]
+        //    secSmallest = smallest
+        }else if (array[i] > smallest && array[i] < secSmallest){
+            secSmallest = array[i]
+       }
+   }
+   return secSmallest
+}
+console.log(smallestt(arr4));
 
 
 
@@ -240,6 +239,29 @@ console.log(frequencyMap(str1))
 //     return sum1/ arr.length
 // }
 // console.log(sum(arr4))
+
+// PROBLEM 6
+
+// let str1 = "hello"
+// let str2 = "aaaaaAAA"
+// let str3 = "More words"
+
+
+// const frequencyMap = (str) => {
+//     let newObj = {};
+//     for(let i = 0; i < str.length; i++){
+//         if (newObj[str[i]]){
+//             newObj[str[i]] +=1
+//              } else {
+//                 newObj[str[i]] = 1
+                
+//             }  
+//         }
+        
+//         return newObj
+//     }
+// console.log(frequencyMap(str1))
+
 
 
 // PROBLEM 7
