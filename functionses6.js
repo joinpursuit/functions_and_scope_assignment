0cbf1c3ff68270126a3a21e864dcb1d630a005ee
trilltown <phil.awich@gmail.com>
2019-10-13 14:47:11 -0400

    Solve prob 8 es6

--- functionses6.js
+++ functionses6.js
@@ -93,8 +93,8 @@
 // ## Question Five
 // Write a function named average that returns the average of an array of numbers
 
-arr1 = [1,2,3,4,5]
-arr2 = [1,42,1,541,42,6]
+// arr1 = [1,2,3,4,5]
+// arr2 = [1,42,1,541,42,6]
 
 // function average(arrNum) {
 //     let sum = 0;
@@ -107,11 +107,119 @@ arr2 = [1,42,1,541,42,6]
 
 // ES6 
 
-const average = (arrNum) => {
-    let sum = 0;
-    for (let i = 0; i < arrNum.length; i++) {
-        sum += arrNum[i];
-    }
-    return sum / arrNum.length;
-}
-console.log(average(arr2))
+// const average = (arrNum) => {
+//     let sum = 0;
+//     for (let i = 0; i < arrNum.length; i++) {
+//         sum += arrNum[i];
+//     }
+//     return sum / arrNum.length;
+// }
+// console.log(average(arr2))
+
+// ## Question Six
+// Write a function named frequencyMap that takes a string as input and returns an object that maps each character its number of occurrences
+
+// function frequencyMap(stringName) {
+//     let characterMap = {}
+//     for(let i =0; i < stringName.length; i++) {
+//         if (characterMap[stringName[i]]) {
+//             characterMap[stringName[i]] ++
+//         }
+//         else {characterMap[stringName[i]] = 1}
+//     }
+// return characterMap
+// }
+// console.log(frequencyMap("hello"))
+
+// ES6
+
+// const frequencyMap = (stringName) => {
+//     let characterMap = {}
+//         for(let i = 0; i < stringName.length; i++) {
+//             if (character[stringName[i]]) {
+//                 characterMap[stringName[i]] ++
+//             }
+//             else {characterMap[stringName[i]] = 1}
+//         }
+//     }
+//     console.log()
+
+//     // 
+
+//     ## Question Seven
+// Write a function called fizzBuzz that logs the numbers from 1 to n as with the following replacements:
+
+// function fizzBuzz(n) {
+//     for (let i = 0; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//             continue;
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//             continue;
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//             continue;
+//         } 
+//         console.log(i);
+//     }
+// }
+
+// fizzBuzz(27);
+
+// const fizzBuzz = (n) => {
+//     for (let i = 0; i < n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//             continue;
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//             continue;
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//             continue;
+//         }
+//         console.log(i);
+//     }
+// }
+// console.log(fizzBuzz(27))
+
+// ## Question Eight
+// Write a function named valueIsGreaterThanAverageOfArr that takes in an array of numbers and a number and returns whether the Double is greater than the averageute
+
+// inputOne = 1
+// inputTwo = [1.0,2,3,4,5]
+
+// function valueIsGreaterThanAverageOfArr(num, array) {
+//     let double = num * 2
+//     let sum = 0
+//     for (let i = 0; i < array.length; i++){
+//         sum += array[i]
+//     }
+//     let avg = sum / array.length
+//     if (double > avg) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(valueIsGreaterThanAverageOfArr(inputOne, inputTwo))
+
+//
+
+// const valueIsGreaterThanAverageOfArr = (num, array) => {
+//     let double = num * 2
+//     let sum = 0
+//     for (let i = 0; i < array.length; i++){
+//         sum += array[i]
+//     } 
+//     let avg = sum / array.length
+//     if (double > avg) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+//  }
+ 
+//  console.log(valueIsGreaterThanAverageOfArr(inputOne, inputTwo))
