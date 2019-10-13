ed34a6a436212b60fd8dd1c2d51e01ed44014f25
dannyl <danilovlaurent@pursuit.org>
2019-10-13 13:01:18 -0400

    problem 7

--- functions.js
+++ functions.js
@@ -54,14 +54,62 @@
 
 // Problem 5
 
-let arr =[1, 2, 3, 4, 5] 
-// let average;
-function sum(arr) {
-    let sum = 0;
-        for (let i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-    return sum/ arr.length
-}
-console.log(sum(arr))
+// let arr  = [1, 2, 3, 4, 5] 
+// let arr2 = [1,42,1,541,42,6]
+// let arr3 = [-10,5,-15,20]
+// let arr4 = [1.5, 2.25, 4.5, -1.5]
 
+// function sum(arr) {
+//     let sum1 = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//     sum1 += arr[i];
+// }
+//     return sum1/ arr.length
+// }
+// console.log(sum(arr4))
+
+
+
+// PROBLEM 6
+
+// let str1 = "hello"
+// let str2 = "aaaaaAAA"
+// let str3 = "More words"
+
+// function frequencyMap(str) {
+//     let newObj = {}
+//    for(let i = 0; i < str.length; i++){
+//    if (str[i] !== i){
+//        newObj[str[i]] = 1
+
+//     } else {
+//     count = newObj[str[i]]++
+
+//    }  
+// //    newObj =   
+//    }
+ 
+// // return newObj
+//  }
+// // console.log(frequencyMap(str1))
+
+
+
+// PROBLEM 7
+
+function fizzBuzz(num) {
+        for (let i = 0; i <= num; i++){
+            if( i % 3 ===0 && i % 5 ===0){
+            console.log("FizzBuzz")
+            }
+            else if ( i % 3 === 0) {
+                console.log("Fizz")
+            } else if ( i % 5 === 0) {
+                console.log("Buzz")
+            } else {
+                console.log(i)
+            } 
+        } 
+    }
+
+console.log(fizzBuzz(27))
