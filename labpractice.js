3becb1c4fb44064fb3812028be9fd7c467f910e7
Danielle R Cherry <daniellecherry@pursuit.org>
2019-10-13 14:40:57 -0400

    Solve Problem 5

--- labpractice.js
+++ labpractice.js
@@ -88,64 +88,16 @@ let occurrences = (character, string) => {
     return characterNum
 }
 
-console.log(occurrences("i", "Mississippi"))
+console.log(occurrences("i", "Mississippi"));
 
 
+//Problem 5
 
-// let arr2 = [1,2,3,4,5]
-// let sum2 = 0
-// for(let i = 0; i < arr2.length; i++) {
-//     sum2 = arr2[i] += arr2[i] 
-//     console.log(sum2)
-// }
-
-
-// function average(arr) {
-//     let sum = 0;
-//     for(let i = 0; i < arr.length; i++) {
-//         sum = arr[i] += arr[i] 
-//         sum/arr.length
-//     }
-//     return sum
-// }
-// console.log(average([1,2,3,4,5]))
-
-
-// function evenNums(arr) {
-//     let newArr = [];
-//     for(let i = 0; i < arr.length; i++) {
-//         if(arr[i] % 2 === 0){
-//             newArr.push(arr[i]);//this how you store info to get return for later
-//     }
-// }
-// return newArr
-// }
-// console.log(evenNums([9,7,10,7,8,13,12]))
-
-
-// // function sayHello() {
-// //     return "hello"
-// // }
-
-// //THIS IS HOW WE WOULD USE ES5
-// var sayHello = function(name) {//you can also wrte a function as a varible to equal the function
-//     return "Hello " + name
-// }
-
-// console.log(sayHello("Danielle")) //a varible takes on the value of whatever it's assigned to
-
-// //
-// const sayHello2 = (name) => {
-//     return "hello " + name//Explict return
-// }
-
-// console.log(sayHello2("Danielle"))
-
-
-// const nums2 = (num1, num2) => {
-//     let newSum = 0;
-//     newSum = num1 + num2;
-//     return newSum
-// }
-// console.log(nums2(45,3))
-
+const average = (arr) => {
+    let sum = 0
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum/arr.length;
+}
+console.log(average([1,42,1,541,42,6]));
