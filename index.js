1f6f1ee94d1492542a2fff3f82caf0b42112cb25
cassidybeni <cassidybeni@pursuit.org>
2020-03-30 22:04:37 -0400

    Complete problem 2

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const double = (num) => {
 }
 
 console.log(double(4))
+
+//Question 2
+
+const smallest = (num1, num2) => {
+    if (num1 < num2) {
+        return num1
+    } else if (num1 > num2) {
+        return num2
+    } else {
+        return num1
+    }
+}
+console.log(smallest(2.3 , 2.03))
