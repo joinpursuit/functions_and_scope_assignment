630f3fd4e48a5b16a0684489df5c71e8984a982d
cassidybeni <cassidybeni@pursuit.org>
2020-04-03 14:32:56 -0400

    Complete problem 4

--- index.js
+++ index.js
@@ -28,4 +28,25 @@ const smallestValueInArr = (arr) => {
 console.log(smallestValueInArr(arr))
 
 //Question 4
+// Write a function named occurrences that counts how many characters in a string match a specific character.
 
+// ```
+// Sample inputs and expected outputs:
+
+// inputOne: "l", inputTwo: "hello", expectedOutput: 2
+// inputOne: "r", inputTwo: "hello world!", expectedOutput: 1
+// inputOne: " ", inputTwo: "Now with some spaces", expectedOutput: 3
+// inputOne: "E", inputTwo: "cApItAlS aRe DiFfErEnT", expectedOutput: 2
+// ```
+
+let char = "E"
+let count = 0
+
+const occurrences = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].includes(char)) 
+    count ++
+  }
+    return count
+}
+console.log(occurrences("cApItAlS aRe DiFfErEnT"))
